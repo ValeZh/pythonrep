1aa40985e7f0257c67e59fb96e67c16f3f370094
ValeZh <mugikaoven@gmil.com>
2023-07-23 16:02:21 +0300

    I changed everything except 8 and 11

--- lab2/main.py
+++ lab2/main.py
@@ -21,7 +21,7 @@ class Films:
         self.fetch_genre()
 
     def fetch_data(self):
-        for i in range(1, int(self.page)):
+        for i in range(1, int(self.page) + 1):
             respons = requests.get(
                 f'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&sort_by=popularity.desc&page={i}',
                 headers=self.header)  # возвращает json с данными
@@ -34,86 +34,50 @@ class Films:
     def give_all_data(self):
         return self.films
 
-    # def give_data_with_index_3_19(self):
-    #     for i in range(3, 19, 4):#slicing
-    #         print(self.films[i])
+    def give_data_with_index_3_19(self):
+        x = slice(3,19,4)
+        return self.films[x]
 
     def the_most_popular(self):# использовать max lst
-        popularity_id = 0
-        maximum_film = 0
+        most_popular_film = max(self.films, key=lambda x:x['popularity'])['original_title']
+        return most_popular_film
 
-        print(len(self.films))
-        for f in range(len(self.films)):
-            if maximum_film < int(self.films[f]['popularity']):
-                maximum_film = int(self.films[f]['popularity'])
-                popularity_id = f
-                print(popularity_id)
-                print(maximum_film)
+    def finde_name_from_discription(self, word = 'planet'):# use comprehension
+        lst_with_word = [f['original_title'] for f in self.films if word in f['overview'] ] # f['original_title'] значение котовые возвращается ( for f in self.films) обычный цикл (if word in f['overview']) если выполняется условие lst_with_word.append(f)????
+        return lst_with_word
 
-        print(f"4.Name of the most popular title {self.films[popularity_id]['original_title']}")
-
-    def finde_name_from_discription(self, word):# use comprehension
-        film_ind_lst = []
-        index = 0
-        for f in self.films:
-            if word in f['overview']:
-                film_ind_lst.append(index)
-            index += 1
-
-        print('5.	Names of titles which has in description key words which a user put as parameters')
-        for answ in film_ind_lst:
-            print(self.films[answ]['original_title'])
 
     def collection_of_genres(self):  # use comprehension
-        answer = set()
-        for f in self.films:
-            answer.add(tuple(f['genre_ids']))
-
-        print('6.	Unique collection of present genres (the collection should not allow inserts)')
-        print(answer)
+        answer = {tuple(f['genre_ids']) for f in self.films }
         return answer
 
-    def delete_film_with_genre(self, id_genre_del):# comprehension илиilter
-        result = []
-        for f in self.films:
-            if id_genre_del not in f['genre_ids']:
-                result.append(f)
-
-        print('7.	Delete all movies with user provided genre')
-        print(result)
+    def delete_film_with_genre(self, id_genre_del):# comprehension или iter, не очень поняла зачем iter
+        result = [f for f in self.films if int(id_genre_del) not in f['genre_ids']]
         return result
 
     def popular_genres(self):
-        lst_for_count = []
-        for f in self.films:#comprehension
-            for ids in f['genre_ids']:
-                lst_for_count.append(ids)
+        lst_for_count = [f for f in self.films for ids in f['genre_ids']]
         cnt = collections.Counter(lst_for_count)
-
-        for g in self.genres:# взять id + name
+        lst_result = [g for g in range(3)]
+        for g in self.genres:# взять id + name я не знаю как по другому
             if g['id'] == cnt.most_common(3)[0][0] or g['id'] == cnt.most_common(3)[1][0] or g['id'] == \
                     cnt.most_common(3)[2][0]:
                 print(g['name'])
 
         print(cnt.most_common(3))
+        return cnt.most_common(3)
+
 
     def group_films_by_genres(self):
-        group_dict = {g['id']: [] for g in self.genres}
-        for f in self.films:# [фильм1, фильм2] - пары по жанру
-            for g in f['genre_ids']:
-                buff = group_dict[g]
-                buff.append(f['original_title'])
-                group_dict.update({g: buff})
-        print(group_dict)
+        group_films1 = {(f['original_title'],k['original_title']) for f in self.films for k in self.films if list(set(f['genre_ids']) & set(k['genre_ids'])) != []}
+        return group_films1
+
 
     def copy_data_with_22(self):# deepcopy
-        copy_data = self.films
-
+        copy_data = deepcopy(self.films)
         for f in copy_data:
             f['genre_ids'][0] = 22
-        print(copy_data)
-        print(self.films)
-        return (copy_data)
+        return copy_data
 
     def make_collection(self, sort):
         result_lst_dic = []
@@ -130,10 +94,10 @@ class Films:
             unchanged_buff_dic = deepcopy(buff_dict)
             result_lst_dic.append(unchanged_buff_dic)
         print(result_lst_dic)
-        if sort == 1:
-            sorted_list = sorted(result_lst_dic, key=lambda x: x['Score'], x['Popularity'])# сортировка по 2 полям
-        print(sorted_list)
-        return (sorted_list)
+#        if sort == 1:
+#            sorted_list = sorted(result_lst_dic, key=lambda x: x['Score'], x['Popularity'])# сортировка по 2 полям
+#        print(sorted_list)
+#        return (sorted_list)
 
     def csv_file_maker(self, coll_dict):
         with open('lab2.txt', mode='w') as csv_file:
@@ -141,8 +105,8 @@ class Films:
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
             writer.writeheader()
-            for d in coll_dict:
-                writer.writerow(d)
+#            for d in coll_dict:
+#                writer.writerow(d)
 
 
 # import pdb;pdb.set_trace()
@@ -150,25 +114,25 @@ page = input()
 answer = Films(page)
 
 # 2 answer
-answer.give_all_data()
+print(f'2.	Give a user all data {answer.give_all_data()}')
 # 3 answers
-#answer.give_data_with_index_3_19()
+print(f'3.	All data about movies with indexes from 3 till 19 with step 4 {answer.give_data_with_index_3_19()}')
 # 4 answer
-#answer.the_most_popular()
+print(f'4.	Name of the most popular title {answer.the_most_popular()}')
 # 5 answer
-# word = input()
-# answer.finde_name_from_discription(word)
+word = input()
+print(f'5.	Names of titles which has in description key words which a user put as parameters {answer.finde_name_from_discription(word)}')
 # 6 answer
-#answer.collection_of_genres()
+print(answer.collection_of_genres())
 # 7 answer
-# id_genre = input()
-# answer.delete_film_with_genre(id_genre)
+id_genre = input()
+print(f'7.	Delete all movies with user provided genre{answer.delete_film_with_genre(id_genre)}')
 # 8 answer
-#answer.popular_genres()
+#print(f'8.	Names of most popular genres with numbers of time they appear in the data {answer.popular_genres()}')
 # 9 answer
-#answer.group_films_by_genres()
+print(f'9.	Collection of film titles grouped in pairs by common genres{answer.group_films_by_genres()}')
 # 10 answer
-answer.copy_data_with_22()
+print(f'10.	Return initial data and copy of initial data where first id in list of film genres was replaced with 22 {answer.copy_data_with_22()}')
 # 11 answer
 dict_coll = answer.make_collection(2)
 # 12 answer
