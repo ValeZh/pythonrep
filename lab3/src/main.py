c1c2e94c003f4681472013b10a219a916069e174
ValeZh <mugikaoven@gmil.com>
2023-08-21 13:06:56 +0300

    some changes

--- lab3/src/main.py
+++ lab3/src/main.py
@@ -9,12 +9,12 @@ main.py --destination_folder='D:\WinUsers\Lera\Documents\summerlabs\pythonrep\la
 
 # использовать argparse
 parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
 parser.add_argument("destination_folder", type=str, required=True)
 parser.add_argument("file_name", type=str, required=True)
-parser.add_argument("gender_filt", type=str)
-group = parser.add_mutually_exclusive_group()
-group.add_argument("--numb_of_rows_filt", type=int)
-group.add_argument("--log_level", type=str)
+group.add_argument("--gender_filt", type=str, required=True)
+group.add_argument("--numb_of_rows_filt", type=int, required=True)
+parser.add_argument("log_level", type=str)
 args = parser.parse_args()
 
 import pdb
@@ -30,22 +30,5 @@ user_data = output_file.struct_data()
 output_file.make_dir_for_decade(user_data)
 output_file.remove_data_before()
 output_file.for_logging_struct()
-
-
 # output_file.zip_maker()
 
-# log_lev = input()
-# print('Destination folder new ')  # D:\WinUsers\Lera\Documents\summerlabs\pythonrep\lab3\strang
-# folder_path = input()
-# print('Filename ')
-# inp_file_name = input()
-#
-# output_file = DataHumans(folder_path, inp_file_name)
-# print('do you want filter by rows or gender')
-# answer = input()
-# if answer == 'gender':
-#     print('print gender')
-#     output_file.filter_by_gender(input())
-# if answer == 'rows':
-#     print('print numb of delete rows')
-#     output_file.delete_rows_for_filt(int(input()))
