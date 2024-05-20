8bca383dacc842bbc7c33a1ef6148b53b5918bf0
ValeZh <mugikaoven@gmail.com>
2024-05-20 18:15:35 +0300

    I make all

--- hw_4/api.py
+++ hw_4/api.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv, find_dotenv
 import logging
 from consts import URL
-from datetime import datetime
+from datetime import datetime, timedelta
 import random
 
 formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
@@ -19,15 +19,13 @@ my_logger.addHandler(my_handler)
 load_dotenv(find_dotenv())
 
 
-# получает дикт из валют и их значений относительно доллара
-def get_curency_data():
-    # .env
-    # dotenv lib
+# gets dict from currencies and their values relative to the dollar
+def get_currency_data():
 
     response = requests.get(URL.format(os.getenv('API_KEY')))
     if response.status_code == requests.codes.ok:
         data = response.json()
-        #сделать логами
+        # make logs
         my_logger.info("data_get successfully")
         my_logger.info(data['data'])
         return data['data']
@@ -35,52 +33,31 @@ def get_curency_data():
     return None
 
 
-# читает цсв файл переобразовывая в дикт
+# reads csv file converted to dict
 def read_csv_to_dict(file_path):
     with open(file_path, 'r') as file:
         return list(csv.DictReader(file))
-
-
-# преобразует лист диктов в дикт листов
-def merge_dicts(dict_list):
-    merged_dict = {}
-    for d in dict_list:
-        for key, value in d.items():
-            # Если ключ уже есть в объединенном словаре, добавляем значение в список
-            if key in merged_dict:
-                merged_dict[key].append(value)
-            # Если ключ новый, создаем список значений
-            else:
-                merged_dict[key] = [value]
-    return merged_dict
-
-
-# проверяет все ли массивы при ключах имеют одинаковую длинну
-# эта функция сделана для заполнения базы данных с помощью командной строк
-# при желании это можно реализовать
-def equal_length_lists(d):
-    lengths = {len(lst) for lst in d.values()}
-    return len(lengths) == 1
 
 
 def unpack_data(data):
     keys = list(data.keys())
     return [data[key] for key in keys] if len(keys) > 1 else data[keys[0]]
 
-#{ filler_date: [{}, {} ]}
-#{ "row1": {}, "row2": {} }
-# заполняет таблицу банк
+
+# { filler_date: [{}, {} ]}
+# { "row1": {}, "row2": {} }
+# fills the table bank
 @db_connection
 def add_bank(cur, **kwargs):
     data = unpack_data(kwargs)
     my_logger.info(data)
-    # распаковать лист диктов
+    # unpack the mass of dicts
     my_logger.info(data)
     cur.executemany("INSERT INTO Bank(name) VALUES(:Name)", data)  # :param_name, :param_name2
     my_logger.info("Bank added successfully.")
 
 
-# заполняет таблицу юзер
+# fills the table user
 @db_connection
 def add_user(cur, **kwargs):
     data = unpack_data(kwargs)
@@ -92,22 +69,24 @@ def add_user(cur, **kwargs):
                     data)
     my_logger.info("User added successfully.")
 
+
 def unpack_to_string(data):
     result = ''
     for tup in data:
         for item in tup:
             result += str(item) + ','
     return result.rstrip(',')
-#
-# заполняет таблицу аккаунт
 
+
+# fills the table account
 def set_accounts(cur, data):
     for i in data:
         cur.execute("SELECT id FROM Account WHERE user_id = ?", (i["user_id"],))
-        numb_of_acc = cur.fetchall()  # Извлекаем значение из курсора
+        numb_of_acc = cur.fetchall()
         account_str = unpack_to_string(numb_of_acc)
         my_logger.info(account_str)
         cur.execute("UPDATE User SET Accounts = ? WHERE id = ?", (account_str, i["user_id"],))
+
 
 @db_connection
 def add_account(cur, **kwargs):
@@ -120,7 +99,8 @@ def add_account(cur, **kwargs):
     set_accounts(cur, data)
     my_logger.info("account added successfully.")
 
-#update row in bd
+
+# update row in bd
 @db_connection
 def update_row(c, table_name, row_id, column, value):
     c.execute(f"UPDATE {table_name} SET {column} = ? WHERE id = ?", (value, row_id, ))
@@ -142,7 +122,7 @@ def delete_row(cur, row_id, fnc):
     return "success"
 
 
-# она просто очищает таблицы в бд
+# it just clears the tables in the database
 @db_connection
 def clear_table(cur, table_name):
     cur.execute(f"DELETE FROM {table_name}")
@@ -163,12 +143,11 @@ def add_table_by_file(path, table_name):
 def convert_currency(currency_values, orig_currency, conv_currency, amount):
     return round((amount / currency_values[orig_currency]) * currency_values[conv_currency], 2)
 
-# достать банкнейм
-@db_connection
-def get_bankname(cur, id_user):
-    bank_id = get_data_from_table(table_name ="Account", row_name="bank_id",row_id= id_user)
+
+def get_bankname(id_user):
+    bank_id = get_data_from_table(table_name="Account", row_name="bank_id", row_id=id_user)
     my_logger.info("get_bankname successfully.")
-    return get_data_from_table(table_name = "Bank",row_name="name",  row_id = bank_id)
+    return get_data_from_table(table_name="Bank", row_name="name",  row_id=bank_id)
 
 
 @db_connection
@@ -178,12 +157,25 @@ def get_data_from_table(cur,  row_name, table_name, row_id):
 
 
 @db_connection
-def transfer_money(c, sender_id, receiver_id, sent_amount, transfer_time=None):
-    currency_dict = get_curency_data()
+def insert_transaction(cur, bank_sender_name, sender_id, bank_receiver_name,
+                       receiver_id, sender_currency, sent_amount, transfer_time):
+    cur.execute('''
+    INSERT INTO BankTransaction
+    (bank_sender_name, account_sender_id, 
+    bank_receiver_name, account_receiver_id, 
+    sent_currency, sent_amount, datetime) 
+    VALUES(?,?,?,?,?,?,?)''',
+                (bank_sender_name, sender_id,
+                 bank_receiver_name, receiver_id,
+                 sender_currency, sent_amount, transfer_time,))
+
+
+def transfer_money(sender_id, receiver_id, sent_amount, transfer_time=None):
+    currency_dict = get_currency_data()
     my_logger.info('valid current success')
-    # вытаскиваем нужные данные для заполнения таблицы транзакций
+    # pull the necessary data to fill the transaction table
     bank_sender_name = get_bankname(sender_id)
-    bank_receiver_name = get_bankname( receiver_id)
+    bank_receiver_name = get_bankname(receiver_id)
     sender_amount = get_data_from_table("Amount", "Account", sender_id)
     receiver_amount = get_data_from_table("Amount", "Account", receiver_id)
     sender_currency = get_data_from_table("Currency", "Account", sender_id)
@@ -191,26 +183,23 @@ def transfer_money(c, sender_id, receiver_id, sent_amount, transfer_time=None):
     sent_am_in_sender_cur = sent_amount
 
     if sender_amount <= sent_amount:
-        raise ValueError('not enough money in the account')
+        raise ValueError("not enough money in the account")
     if receiver_currency != sender_currency:
         sent_am_in_sender_cur = convert_currency(currency_dict, receiver_currency, sender_currency, sent_amount)
 
-    # проверяем наличие времени
+    # check the time
     transfer_time = validate.add_transaction_time(transfer_time)
 
     new_sender_amount = sender_amount - sent_am_in_sender_cur
     new_receiver_amount = receiver_amount + sent_am_in_sender_cur
 
-    # меняем значения суммы у юзеров
-    update_row('Account', sender_id, 'amount', round(new_sender_amount,2))
-    update_row('Account', receiver_id, 'amount', round(new_receiver_amount,2))
+    # change the sum values of the users
+    update_row("Account", sender_id, "amount", round(new_sender_amount, 2))
+    update_row("Account", receiver_id, "amount", round(new_receiver_amount, 2))
 
-    # вынести функцией
-    # заполняем таблицу
-    c.execute(
-        '''INSERT INTO BankTransaction(bank_sender_name, account_sender_id, bank_receiver_name, account_receiver_id, 
-        sent_currency, sent_amount, datetime) VALUES(?,?,?,?,?,?,?)''',
-        (bank_sender_name, sender_id, bank_receiver_name, receiver_id, sender_currency, sent_amount, transfer_time,))
+    # fill the table
+    insert_transaction(bank_sender_name, sender_id, bank_receiver_name, receiver_id,
+                       sender_currency, sent_amount, transfer_time)
     my_logger.info("success")
     return "success"
 
@@ -238,7 +227,7 @@ def user_with_highest_amount(cursor):
         LIMIT 1
     ''')
     user_id = cursor.fetchone()[0]
-    name = get_data_from_table("name","User", user_id)
+    name = get_data_from_table("name", "User", user_id)
     my_logger.info(name)
     my_logger.info("user_with_highest_amount success")
     return name
@@ -246,15 +235,15 @@ def user_with_highest_amount(cursor):
 
 @db_connection
 def bank_with_biggest_capital(cursor):
-    currency_dict = get_curency_data()
-    # Извлекаем все записи из таблицы Account
+    currency_dict = get_currency_data()
+    # Extract all records from the Account table
     cursor.execute("SELECT Bank_id, Currency, Amount FROM Account")
     accounts = cursor.fetchall()
 
-    # Создаем словарь для хранения суммарного капитала для каждого банка в долларах
+    # Create a dictionary to store total capital for each bank in dollars
     bank_capital = {}
 
-    # Заполняем словарь данными из таблицы Account
+    # Create a dictionary to store the total capital for each bank in dollars
     for bank_id, currency, amount in accounts:
         amount_in_usd = convert_currency(currency_dict, currency, 'USD', amount)
         if bank_id in bank_capital:
@@ -262,24 +251,22 @@ def bank_with_biggest_capital(cursor):
         else:
             bank_capital[bank_id] = amount_in_usd
 
-    # Находим банк с наибольшим капиталом
+    # Find the bank with the most capital
     max_capital_bank_id = max(bank_capital, key=bank_capital.get)
-
-    # Возвращаем ID банка с наибольшим капиталом
+    my_logger.info(f"max capital bank id {max_capital_bank_id}")
+    my_logger.info("bank_with_biggest_capital success")
     return max_capital_bank_id
 
 
 @db_connection
 def bank_serving_oldest_client(cursor):
-    # Извлекаем все записи из таблицы User
     cursor.execute("SELECT Id, Birth_day FROM User")
     users = cursor.fetchall()
 
-    # Извлекаем все записи из таблицы Account
     cursor.execute("SELECT User_id, Bank_id FROM Account")
     accounts = cursor.fetchall()
 
-    # Находим самого старого пользователя
+    # Find the oldest user
     oldest_user_id = None
     oldest_birth_day = datetime.max
 
@@ -289,7 +276,7 @@ def bank_serving_oldest_client(cursor):
             oldest_birth_day = birth_date
             oldest_user_id = user_id
 
-    # Находим банк, которому принадлежит самый старый пользователь
+    # Find the bank that owns the oldest user
     for user_id, bank_id in accounts:
         if user_id == oldest_user_id:
             return bank_id
@@ -309,30 +296,54 @@ def print_table(cursor, table_name):
 
 @db_connection
 def bank_with_highest_unique_users(cursor):
-    # Создаем словарь для хранения количества уникальных пользователей для каждого банка
+    # Create a dictionary to store the number of unique users for each bank
     unique_users_by_bank = {}
 
-    # Выбираем все транзакции из таблицы BankTransaction
+    # Select all transactions from the BankTransaction table
     cursor.execute("SELECT Bank_sender_name, Account_sender_id FROM BankTransaction")
     transactions = cursor.fetchall()
 
-    # Подсчитываем количество уникальных пользователей для каждого банка
+    # Count the number of unique users for each bank
     for bank_name, account_id in transactions:
         if bank_name not in unique_users_by_bank:
             unique_users_by_bank[bank_name] = set()
         unique_users_by_bank[bank_name].add(account_id)
 
-    # Находим банк с наибольшим количеством уникальных пользователей
+    # Find the bank with the unique users
     bank_with_highest_users = max(unique_users_by_bank.items(), key=lambda x: len(x[1]))
 
     return bank_with_highest_users[0]
 
+
 @db_connection
 def delete_users_with_incomplete_info(cursor):
-    cursor.execute("SELECT id FROM User WHERE Name IS NULL OR Surname IS NULL OR Birth_day IS NULL")
-    deleted_user_id = cursor.fetchall()
-    cursor.execute("DELETE FROM Account WHERE User_id = ?", deleted_user_id)
+    while True:
+        cursor.execute("SELECT id FROM User WHERE Name IS NULL OR Surname IS NULL OR Birth_day IS NULL")
+        result = cursor.fetchone()
+        if result is None:
+            break
+        deleted_user_id = result[0]
+        cursor.execute("DELETE FROM Account WHERE User_id = ?", (deleted_user_id,))
+        cursor.execute("DELETE FROM User WHERE id = ?", (deleted_user_id,))
     return "Deletion complete"
 
 
+@db_connection
+def get_user_transactions(c, user_id):
+
+    # Calculate the date three months ago from today
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=90)
+
+    # Convert dates to string format for SQL query
+    start_date_str = start_date.strftime('%Y-%m-%d %H:%M:%S')
+    end_date_str = end_date.strftime('%Y-%m-%d %H:%M:%S')
+
+    query = '''
+    SELECT * FROM BankTransaction
+    WHERE (Account_sender_id = ? OR Account_receiver_id = ?)
+    AND Datetime BETWEEN ? AND ?
+    '''
+    c.execute(query, (user_id, user_id, start_date_str, end_date_str))
+    return c.fetchall()
  
--- hw_4/main.py
+++ hw_4/main.py
@@ -18,13 +18,19 @@ def init_args():
     parser.add_argument('--time', type=str)
     parser.add_argument('--clear_table', type=str)
     parser.add_argument('--random_disc', type=str)
+    parser.add_argument('--highest_amount', type=str)
+    parser.add_argument('--bank_with_biggest_capital', type=str)
+    parser.add_argument('--bank_with_highest_unique_users', type=str)
+    parser.add_argument('--get_user_transactions', type=int)
+    parser.add_argument('--print_table', type=str)
+    parser.add_argument('--delete_incomplete', type=str)
     args = parser.parse_args()
 
     return args
 
 
 def do_func_with_args(args):
-    # чтоб можно было выполнять несколько функций
+    # to be able to perform multiple functions
     if args.add_path and args.table_name:
         api.add_table_by_file(args.add_path, args.table_name)
     if args.table_name and args.row_id and args.column and args.value:
@@ -32,21 +38,25 @@ def do_func_with_args(args):
     if args.clear_table:
         api.clear_table(args.clear_table)
     if args.delete_row_id and args.table_name:
-        api.delete_row(args.delete_row_id ,args.table_name)
+        api.delete_row(args.delete_row_id, args.table_name)
     if args.sender_id and args.receiver_id and args.sent_amount:
         api.transfer_money(args.sender_id, args.receiver_id, args.sent_amount)
     if args.sender_id and args.receiver_id and args.sent_amount and args.time:
         api.transfer_money(args.sender_id, args.receiver_id, args.sent_amount, args.time)
     if args.random_disc:
         print(api.select_random_users_with_discounts())
-
-    print(api.user_with_highest_amount())
-    # Пример использования
-    print(api.bank_serving_oldest_client())
-    print(api.bank_with_biggest_capital())
-    print(api.bank_with_highest_unique_users())
-    api.print_table("BankTransaction")
-
+    if args.highest_amount:
+        print(api.user_with_highest_amount())
+    if args.bank_with_biggest_capital:
+        print(api.bank_with_biggest_capital())
+    if args.bank_with_highest_unique_users:
+        print(api.bank_with_highest_unique_users())
+    if args.get_user_transactions:
+        print(api.get_user_transactions())
+    if args.print_table:
+        api.print_table(args.print_table)
+    if args.delete_incomplete:
+        print(api.delete_users_with_incomplete_info())
 
 
 def main():
